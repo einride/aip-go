0743b4ffa93a59e4a94d7cc2ba277549c179a0f2
Oscar Söderlund <odsod@users.noreply.github.com>
2020-12-28 13:27:20 +0100

    feat: add fieldmask package with UpdateTopLevelFields
    Supports implementation of standard Update methods where only top-level
    fields are supported by the update mask.

---
+++ fieldmask/doc.go
@@ -0,0 +1,8 @@
+// Package fieldmask provides primitives for implementing AIP field mask functionality.
+//
+// This package only provides primitives not already provided by the package
+// google.golang.org/protobuf/types/known/fieldmaskpb.
+//
+// See: https://google.aip.dev/134 (Standard methods: Update)
+// See: https://google.aip.dev/157 (Partial responses)
+package fieldmask
