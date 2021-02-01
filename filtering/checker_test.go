c8b00bbe13f109cbde284110fea811d73cb33bde
Oscar Söderlund <odsod@users.noreply.github.com>
2021-02-01 08:05:50 +0100

    feat: initial implementation of filtering package
    Based on the filtering EBNF grammar.
    
    Inspired by a combination of grafeas (parsing) and cel-go (type
    checking).

---
+++ filtering/checker_test.go
@@ -0,0 +1,406 @@
+package filtering
+
+import (
+	"testing"
+
+	syntaxv1 "go.einride.tech/aip/examples/proto/gen/einride/example/syntax/v1"
+	"gotest.tools/v3/assert"
+)
+
+func TestChecker(t *testing.T) {
+	t.Parallel()
+	for _, tt := range []struct {
+		filter        string
+		declarations  []DeclarationOption
+		errorContains string
+	}{
+		{
+			filter: "New York Giants",
+			declarations: []DeclarationOption{
+				DeclareIdent("New", TypeBool),
+				DeclareIdent("York", TypeBool),
+				DeclareIdent("Giants", TypeBool),
+			},
+			errorContains: "undeclared function 'FUZZY'",
+		},
+
+		{
+			filter: "New York Giants OR Yankees",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("New", TypeBool),
+				DeclareIdent("York", TypeBool),
+				DeclareIdent("Giants", TypeBool),
+				DeclareIdent("Yankees", TypeBool),
+			},
+			errorContains: "undeclared function 'FUZZY'",
+		},
+
+		{
+			filter: "New York (Giants OR Yankees)",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("New", TypeBool),
+				DeclareIdent("York", TypeBool),
+				DeclareIdent("Giants", TypeBool),
+				DeclareIdent("Yankees", TypeBool),
+			},
+			errorContains: "undeclared function 'FUZZY'",
+		},
+
+		{
+			filter: "a b AND c AND d",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("a", TypeBool),
+				DeclareIdent("b", TypeBool),
+				DeclareIdent("c", TypeBool),
+				DeclareIdent("d", TypeBool),
+			},
+			errorContains: "undeclared function 'FUZZY'",
+		},
+
+		{
+			filter: "a",
+			declarations: []DeclarationOption{
+				DeclareIdent("a", TypeBool),
+			},
+		},
+
+		{
+			filter: "(a b) AND c AND d",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("a", TypeBool),
+				DeclareIdent("b", TypeBool),
+				DeclareIdent("c", TypeBool),
+				DeclareIdent("d", TypeBool),
+			},
+			errorContains: "undeclared function 'FUZZY'",
+		},
+
+		{
+			filter: `author = "Karin Boye" AND NOT read`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("author", TypeString),
+				DeclareIdent("read", TypeBool),
+			},
+		},
+
+		{
+			filter: "a < 10 OR a >= 100",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("a", TypeInt),
+			},
+		},
+
+		{
+			filter: "NOT (a OR b)",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("a", TypeBool),
+				DeclareIdent("b", TypeBool),
+			},
+		},
+
+		{
+			filter: `-file:".java"`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("file", TypeString),
+			},
+		},
+
+		{
+			filter:        "-30",
+			errorContains: "non-bool result type",
+		},
+
+		{
+			filter: "package=com.google",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("package", TypeString),
+				DeclareIdent("com", TypeMap(TypeString, TypeString)),
+			},
+		},
+
+		{
+			filter: `msg != 'hello'`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("msg", TypeString),
+			},
+		},
+
+		{
+			filter: `1 > 0`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+			},
+		},
+
+		{
+			filter: `2.5 >= 2.4`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+			},
+		},
+
+		{
+			filter: `foo >= -2.4`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("foo", TypeFloat),
+			},
+		},
+
+		{
+			filter: `foo >= (-2.4)`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("foo", TypeFloat),
+			},
+		},
+
+		{
+			filter: `-2.5 >= -2.4`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+			},
+		},
+
+		{
+			filter: `yesterday < request.time`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("yesterday", TypeTimestamp),
+			},
+			// TODO: Add support for structs.
+			errorContains: "undeclared identifier 'request'",
+		},
+
+		{
+			filter: `experiment.rollout <= cohort(request.user)`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareFunction("cohort", NewFunctionOverload("cohort_string", TypeFloat, TypeString)),
+			},
+			// TODO: Add support for structs.
+			errorContains: "undeclared identifier 'experiment'",
+		},
+
+		{
+			filter: `prod`,
+			declarations: []DeclarationOption{
+				DeclareIdent("prod", TypeBool),
+			},
+		},
+
+		{
+			filter: `expr.type_map.1.type`,
+			// TODO: Add support for structs.
+			errorContains: "undeclared identifier 'expr'",
+		},
+
+		{
+			filter: `regex(m.key, '^.*prod.*$')`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("m", TypeMap(TypeString, TypeString)),
+				DeclareFunction("regex", NewFunctionOverload("regex_string", TypeBool, TypeString, TypeString)),
+			},
+		},
+
+		{
+			filter: `math.mem('30mb')`,
+			declarations: []DeclarationOption{
+				DeclareFunction("math.mem", NewFunctionOverload("math.mem_string", TypeBool, TypeString)),
+			},
+		},
+
+		{
+			filter: `(msg.endsWith('world') AND retries < 10)`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("retries", TypeInt),
+			},
+			errorContains: "undeclared function 'msg.endsWith'",
+		},
+
+		{
+			filter: `(endsWith(msg, 'world') AND retries < 10)`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareFunction("endsWith", NewFunctionOverload("endsWith_string", TypeBool, TypeString, TypeString)),
+				DeclareIdent("retries", TypeInt),
+				DeclareIdent("msg", TypeString),
+			},
+		},
+
+		{
+			filter: "expire_time > time.now()",
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareFunction("time.now", NewFunctionOverload("time.now", TypeTimestamp)),
+				DeclareIdent("expire_time", TypeTimestamp),
+			},
+		},
+
+		{
+			filter: `time.now() > timestamp("2012-04-21T11:30:00-04:00")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareFunction("time.now", NewFunctionOverload("time.now", TypeTimestamp)),
+			},
+		},
+
+		{
+			filter: `time.now() > timestamp("INVALID")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareFunction("time.now", NewFunctionOverload("time.now", TypeTimestamp)),
+			},
+			errorContains: "invalid timestamp",
+		},
+
+		{
+			filter: `ttl > duration("30s")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("ttl", TypeDuration),
+			},
+		},
+
+		{
+			filter: `ttl > duration("INVALID")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("ttl", TypeDuration),
+			},
+			errorContains: "invalid duration",
+		},
+
+		{
+			filter: `ttl > duration(input_field)`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("ttl", TypeDuration),
+				DeclareIdent("input_field", TypeString),
+			},
+		},
+
+		{
+			filter: `create_time > timestamp("2006-01-02T15:04:05+07:00")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("create_time", TypeTimestamp),
+			},
+		},
+
+		{
+			filter: `create_time > timestamp("2006-01-02T15:04:05+07:00")`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("create_time", TypeTimestamp),
+			},
+		},
+
+		{
+			filter: `
+				start_time > timestamp("2006-01-02T15:04:05+07:00") AND
+				(driver = "driver1" OR start_driver = "driver1" OR end_driver = "driver1")
+			`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("start_time", TypeTimestamp),
+				DeclareIdent("driver", TypeString),
+				DeclareIdent("start_driver", TypeString),
+				DeclareIdent("end_driver", TypeString),
+			},
+		},
+
+		{
+			filter: `annotations:schedule`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("annotations", TypeMap(TypeString, TypeString)),
+			},
+		},
+
+		{
+			filter: `annotations.schedule = "test"`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareIdent("annotations", TypeMap(TypeString, TypeString)),
+			},
+		},
+
+		{
+			filter: `enum = ENUM_ONE`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareEnumIdent("enum", syntaxv1.Enum(0).Type()),
+			},
+		},
+
+		{
+			filter: `enum = ENUM_ONE AND NOT enum2 = ENUM_TWO`,
+			declarations: []DeclarationOption{
+				DeclareStandardFunctions(),
+				DeclareEnumIdent("enum", syntaxv1.Enum(0).Type()),
+				DeclareEnumIdent("enum2", syntaxv1.Enum(0).Type()),
+			},
+		},
+
+		{
+			filter:        "<",
+			errorContains: "unexpected token <",
+		},
+
+		{
+			filter:        `(-2.5) >= -2.4`,
+			errorContains: "unexpected token >=",
+		},
+
+		{
+			filter:        `a = "foo`,
+			errorContains: "unterminated string",
+		},
+
+		{
+			filter:        "invalid = foo\xa0\x01bar",
+			errorContains: "invalid UTF-8",
+		},
+	} {
+		tt := tt
+		t.Run(tt.filter, func(t *testing.T) {
+			t.Parallel()
+			var parser Parser
+			parser.Init(tt.filter)
+			parsedExpr, err := parser.Parse()
+			if err != nil && tt.errorContains != "" {
+				assert.ErrorContains(t, err, tt.errorContains)
+				return
+			}
+			assert.NilError(t, err)
+			declarations, err := NewDeclarations(tt.declarations...)
+			if err != nil && tt.errorContains != "" {
+				assert.ErrorContains(t, err, tt.errorContains)
+				return
+			}
+			assert.NilError(t, err)
+			var checker Checker
+			checker.Init(parsedExpr, declarations)
+			checkedExpr, err := checker.Check()
+			if tt.errorContains != "" {
+				assert.ErrorContains(t, err, tt.errorContains)
+				return
+			}
+			assert.NilError(t, err)
+			assert.Assert(t, checkedExpr != nil)
+		})
+	}
+}
---
+++ filtering/parser_test.go
@@ -0,0 +1,257 @@
+package filtering
+
+import (
+	"testing"
+	"time"
+
+	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
+	"google.golang.org/protobuf/testing/protocmp"
+	"gotest.tools/v3/assert"
+)
+
+func TestParser(t *testing.T) {
+	t.Parallel()
+	for _, tt := range []struct {
+		filter        string
+		expected      *expr.Expr
+		errorContains string
+	}{
+		{
+			filter:   "New York Giants",
+			expected: Sequence(Text("New"), Text("York"), Text("Giants")),
+		},
+
+		{
+			filter:   "New York Giants OR Yankees",
+			expected: Sequence(Text("New"), Text("York"), Or(Text("Giants"), Text("Yankees"))),
+		},
+
+		{
+			filter:   "New York (Giants OR Yankees)",
+			expected: Sequence(Text("New"), Text("York"), Or(Text("Giants"), Text("Yankees"))),
+		},
+
+		{
+			filter: "a b AND c AND d",
+			expected: And(
+				Sequence(Text("a"), Text("b")),
+				Text("c"),
+				Text("d"),
+			),
+		},
+
+		{
+			filter: "(a b) AND c AND d",
+			expected: And(
+				Sequence(Text("a"), Text("b")),
+				Text("c"),
+				Text("d"),
+			),
+		},
+
+		{
+			filter: "a < 10 OR a >= 100",
+			expected: Or(
+				LessThan(Text("a"), Int(10)),
+				GreaterEquals(Text("a"), Int(100)),
+			),
+		},
+
+		{
+			filter:   "NOT (a OR b)",
+			expected: Not(Or(Text("a"), Text("b"))),
+		},
+
+		{
+			filter:   `-file:".java"`,
+			expected: Not(Has(Text("file"), String(".java"))),
+		},
+
+		{
+			filter:   "-30",
+			expected: Int(-30),
+		},
+
+		{
+			filter:   "package=com.google",
+			expected: Equals(Text("package"), Member(Text("com"), "google")),
+		},
+
+		{
+			filter:   `msg != 'hello'`,
+			expected: NotEquals(Text("msg"), String("hello")),
+		},
+
+		{
+			filter:   `1 > 0`,
+			expected: GreaterThan(Int(1), Int(0)),
+		},
+
+		{
+			filter:   `2.5 >= 2.4`,
+			expected: GreaterEquals(Float(2.5), Float(2.4)),
+		},
+
+		{
+			filter:   `foo >= -2.4`,
+			expected: GreaterEquals(Text("foo"), Float(-2.4)),
+		},
+
+		{
+			filter:   `foo >= (-2.4)`,
+			expected: GreaterEquals(Text("foo"), Float(-2.4)),
+		},
+
+		{
+			filter:   `-2.5 >= -2.4`,
+			expected: Not(GreaterEquals(Float(2.5), Float(-2.4))),
+		},
+
+		{
+			filter:   `yesterday < request.time`,
+			expected: LessThan(Text("yesterday"), Member(Text("request"), "time")),
+		},
+
+		{
+			filter: `experiment.rollout <= cohort(request.user)`,
+			expected: LessEquals(
+				Member(Text("experiment"), "rollout"),
+				Function("cohort", Member(Text("request"), "user")),
+			),
+		},
+
+		{
+			filter:   `prod`,
+			expected: Text("prod"),
+		},
+
+		{
+			filter:   `expr.type_map.1.type`,
+			expected: Member(Member(Member(Text("expr"), "type_map"), "1"), "type"),
+		},
+
+		{
+			filter:   `regex(m.key, '^.*prod.*$')`,
+			expected: Function("regex", Member(Text("m"), "key"), String("^.*prod.*$")),
+		},
+
+		{
+			filter:   `math.mem('30mb')`,
+			expected: Function("math.mem", String("30mb")),
+		},
+
+		{
+			filter: `(msg.endsWith('world') AND retries < 10)`,
+			expected: And(
+				Function("msg.endsWith", String("world")),
+				LessThan(Text("retries"), Int(10)),
+			),
+		},
+
+		{
+			filter: `(endsWith(msg, 'world') AND retries < 10)`,
+			expected: And(
+				Function("endsWith", Text("msg"), String("world")),
+				LessThan(Text("retries"), Int(10)),
+			),
+		},
+
+		{
+			filter:   "time.now()",
+			expected: Function("time.now"),
+		},
+
+		{
+			filter: `timestamp("2012-04-21T11:30:00-04:00")`,
+			expected: Timestamp(
+				time.Date(2012, time.April, 21, 11, 30, 0, 0, time.FixedZone("EST", -4*int(time.Hour.Seconds()))),
+			),
+		},
+
+		{
+			filter:   `duration("32s")`,
+			expected: Duration(32 * time.Second),
+		},
+
+		{
+			filter:   `duration("4h0m0s")`,
+			expected: Duration(4 * time.Hour),
+		},
+
+		{
+			filter: `
+				start_time > timestamp("2006-01-02T15:04:05+07:00") AND 
+				(driver = "driver1" OR start_driver = "driver1" OR end_driver = "driver1")
+			`,
+			expected: And(
+				GreaterThan(
+					Text("start_time"),
+					Timestamp(
+						time.Date(
+							2006, time.January, 2, 15, 4, 5, 0,
+							time.FixedZone("EST", 7*int(time.Hour.Seconds())),
+						),
+					),
+				),
+				Or(
+					Equals(Text("driver"), String("driver1")),
+					Equals(Text("start_driver"), String("driver1")),
+					Equals(Text("end_driver"), String("driver1")),
+				),
+			),
+		},
+
+		{
+			filter:   `annotations:schedule`,
+			expected: Has(Text("annotations"), String("schedule")),
+		},
+
+		{
+			filter:   `annotations.schedule = "test"`,
+			expected: Equals(Member(Text("annotations"), "schedule"), String("test")),
+		},
+
+		{
+			filter:        "<",
+			errorContains: "unexpected token <",
+		},
+
+		{
+			filter:        `(-2.5) >= -2.4`,
+			errorContains: "unexpected token >=",
+		},
+
+		{
+			filter:        `a = "foo`,
+			errorContains: "unterminated string",
+		},
+
+		{
+			filter:        "invalid = foo\xa0\x01bar",
+			errorContains: "invalid UTF-8",
+		},
+	} {
+		tt := tt
+		t.Run(tt.filter, func(t *testing.T) {
+			t.Parallel()
+			var parser Parser
+			parser.Init(tt.filter)
+			actual, err := parser.Parse()
+			if tt.errorContains != "" {
+				if actual != nil {
+					t.Log(actual.Expr.String())
+				}
+				assert.ErrorContains(t, err, tt.errorContains)
+			} else {
+				assert.NilError(t, err)
+				assert.DeepEqual(
+					t,
+					tt.expected,
+					actual.Expr,
+					protocmp.Transform(),
+					protocmp.IgnoreFields(&expr.Expr{}, "id"),
+				)
+			}
+		})
+	}
+}
