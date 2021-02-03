eecab086d377befd80c1cc910a2f17e48def3d84
ericwenn <ericwenn@users.noreply.github.com>
2021-02-03 14:08:15 +0100

    feat: handle nested messages in fieldmask
    `Update` updates fields in destination message with values from source
    message recursively.

--- fieldmask/update.go
+++ fieldmask/update.go
@@ -53,3 +53,75 @@ func UpdateTopLevelFields(mask *fieldmaskpb.FieldMask, dst, src proto.Message) {
 		}
 	}
 }
+
+// Update updates fields in dst with values from src according to the provided field mask.
+// Nested messages are recursively updated in the same manner.
+// Repeated fields and maps are copied by reference from src to dst.
+//
+// If no update mask is provided, only non-zero values of src are copied to dst.
+// If the special value "*" is provided as the field mask, a full replacement of all fields in dst is done.
+//
+// See: https://google.aip.dev/134 (Standard methods: Update).
+func Update(mask *fieldmaskpb.FieldMask, dst, src proto.Message) {
+	dstReflect := dst.ProtoReflect()
+	srcReflect := src.ProtoReflect()
+	if dstReflect.Descriptor() != srcReflect.Descriptor() {
+		panic(fmt.Sprintf(
+			"dst (%s) and src (%s) messages have different types",
+			dstReflect.Descriptor().FullName(),
+			srcReflect.Descriptor().FullName(),
+		))
+	}
+	switch {
+	// Special-case: No update mask.
+	// Update all fields of src that are set on the wire.
+	case len(mask.GetPaths()) == 0:
+		updateWireSetFields(dstReflect, srcReflect)
+	// Special-case: Update mask is [*].
+	// Do a full replacement of all fields.
+	case len(mask.GetPaths()) == 1 && mask.GetPaths()[0] == "*":
+		proto.Reset(dst)
+		proto.Merge(dst, src)
+	default:
+		for _, path := range mask.GetPaths() {
+			segments := strings.Split(path, ".")
+			updateNamedField(dstReflect, srcReflect, segments)
+		}
+	}
+}
+
+func updateWireSetFields(dst, src protoreflect.Message) {
+	src.Range(func(field protoreflect.FieldDescriptor, value protoreflect.Value) bool {
+		if isMessage(field) {
+			updateWireSetFields(dst.Get(field).Message(), value.Message())
+			return true
+		}
+		dst.Set(field, value)
+		return true
+	})
+}
+
+func updateNamedField(dst, src protoreflect.Message, segments []string) {
+	if len(segments) == 0 {
+		return
+	}
+	field := src.Descriptor().Fields().ByName(protoreflect.Name(segments[0]))
+	if field == nil {
+		return // no known field by that name
+	}
+	// a field in this message
+	if len(segments) == 1 {
+		dst.Set(field, src.Get(field))
+		return
+	}
+	if !isMessage(field) {
+		// not a message so can not have a field with that name
+		return
+	}
+	updateNamedField(dst.Get(field).Message(), src.Get(field).Message(), segments[1:])
+}
+
+func isMessage(field protoreflect.FieldDescriptor) bool {
+	return (field.Kind() == protoreflect.MessageKind || field.Kind() == protoreflect.GroupKind) &&
+		!field.IsMap() && !field.IsList()
+}
--- fieldmask/update_test.go
+++ fieldmask/update_test.go
@@ -168,3 +168,313 @@ func TestUpdateTopLevelFields(t *testing.T) {
 		}
 	})
 }
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+	t.Run("should panic on different src and dst", func(t *testing.T) {
+		t.Parallel()
+		assert.Assert(t, cmp.Panics(func() {
+			Update(&fieldmaskpb.FieldMask{}, &library.Book{}, &library.Shelf{})
+		}))
+	})
+
+	t.Run("test cases", func(t *testing.T) {
+		t.Parallel()
+		for _, tt := range []struct {
+			name     string
+			mask     *fieldmaskpb.FieldMask
+			src      proto.Message
+			dst      proto.Message
+			expected proto.Message
+		}{
+			{
+				name: "full replacement",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"*"},
+				},
+				src: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "src-title",
+					Read:   false,
+				},
+				dst: &library.Book{
+					Name:   "dst-name",
+					Author: "dst-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+				expected: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "src-title",
+					Read:   false,
+				},
+			},
+			{
+				name: "full replacement: nested",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"*"},
+				},
+				src: &library.CreateBookRequest{
+					Name: "src-name",
+					Book: &library.Book{
+						Author: "src-author",
+						Read:   false,
+					},
+				},
+				dst: &library.CreateBookRequest{
+					Name: "dst-name",
+					Book: &library.Book{
+						Author: "dst-author",
+						Title:  "dst-title",
+						Read:   true,
+					},
+				},
+				expected: &library.CreateBookRequest{
+					Name: "src-name",
+					Book: &library.Book{
+						Author: "src-author",
+						Read:   false,
+					},
+				},
+			},
+
+			{
+				name: "no field mask replaces non-zero fields",
+				mask: nil,
+				src: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+				},
+				dst: &library.Book{
+					Name:   "dst-name",
+					Author: "dst-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+				expected: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+			},
+			{
+				name: "no field mask replaces non-zero fields: nested",
+				mask: nil,
+				src: &library.CreateBookRequest{
+					Book: &library.Book{
+						Name:   "src-name",
+						Author: "src-author",
+					},
+				},
+				dst: &library.CreateBookRequest{
+					Book: &library.Book{
+						Name:   "dst-name",
+						Author: "dst-author",
+						Title:  "dst-title",
+						Read:   true,
+					},
+				},
+				expected: &library.CreateBookRequest{
+					Book: &library.Book{
+						Name:   "src-name",
+						Author: "src-author",
+						Title:  "dst-title",
+						Read:   true,
+					},
+				},
+			},
+
+			{
+				name: "with partial field mask",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"name", "author", "read"},
+				},
+				src: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "src-title",
+					Read:   false,
+				},
+				dst: &library.Book{
+					Name:   "dst-name",
+					Author: "dst-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+				expected: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "dst-title",
+					Read:   false,
+				},
+			},
+
+			{
+				name: "with non-top-level path",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"name", "book.name"},
+				},
+				src: &library.CreateBookRequest{
+					Name: "src-shelf",
+					Book: &library.Book{
+						Name:   "src-name",
+						Author: "src-author",
+						Title:  "src-title",
+						Read:   false,
+					},
+				},
+				dst: &library.CreateBookRequest{
+					Name: "dst-shelf",
+					Book: &library.Book{
+						Name:   "dst-name",
+						Author: "dst-author",
+						Title:  "dst-title",
+						Read:   true,
+					},
+				},
+				expected: &library.CreateBookRequest{
+					Name: "src-shelf",
+					Book: &library.Book{
+						Name:   "src-name",
+						Author: "dst-author",
+						Title:  "dst-title",
+						Read:   true,
+					},
+				},
+			},
+
+			{
+				name: "repeated field",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"books"},
+				},
+				src: &library.ListBooksResponse{
+					NextPageToken: "xxx",
+					Books: []*library.Book{
+						{
+							Name:   "src-name",
+							Author: "src-author",
+							Title:  "src-title",
+							Read:   false,
+						},
+						{
+							Name:   "src-name-2",
+							Author: "src-author-2",
+							Title:  "src-title-2",
+							Read:   false,
+						},
+					},
+				},
+				dst: &library.ListBooksResponse{
+					NextPageToken: "yyy",
+					Books: []*library.Book{
+						{
+							Name:   "dst-name",
+							Author: "dst-author",
+							Title:  "dst-title",
+							Read:   false,
+						},
+					},
+				},
+				expected: &library.ListBooksResponse{
+					NextPageToken: "yyy",
+					Books: []*library.Book{
+						{
+							Name:   "src-name",
+							Author: "src-author",
+							Title:  "src-title",
+							Read:   false,
+						},
+						{
+							Name:   "src-name-2",
+							Author: "src-author-2",
+							Title:  "src-title-2",
+							Read:   false,
+						},
+					},
+				},
+			},
+
+			{
+				name: "repeated field nested path",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"books.name"},
+				},
+				src: &library.ListBooksResponse{
+					NextPageToken: "xxx",
+					Books: []*library.Book{
+						{
+							Name:   "src-name",
+							Author: "src-author",
+							Title:  "src-title",
+							Read:   false,
+						},
+						{
+							Name:   "src-name-2",
+							Author: "src-author-2",
+							Title:  "src-title-2",
+							Read:   false,
+						},
+					},
+				},
+				dst: &library.ListBooksResponse{
+					NextPageToken: "yyy",
+					Books: []*library.Book{
+						{
+							Name:   "dst-name",
+							Author: "dst-author",
+							Title:  "dst-title",
+							Read:   false,
+						},
+					},
+				},
+				expected: &library.ListBooksResponse{
+					NextPageToken: "yyy",
+					Books: []*library.Book{
+						{
+							Name:   "dst-name",
+							Author: "dst-author",
+							Title:  "dst-title",
+							Read:   false,
+						},
+					},
+				},
+			},
+
+			{
+				name: "with unknown path",
+				mask: &fieldmaskpb.FieldMask{
+					Paths: []string{"name", "foo"},
+				},
+				src: &library.Book{
+					Name:   "src-name",
+					Author: "src-author",
+					Title:  "src-title",
+					Read:   false,
+				},
+				dst: &library.Book{
+					Name:   "dst-name",
+					Author: "dst-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+				expected: &library.Book{
+					Name:   "src-name",
+					Author: "dst-author",
+					Title:  "dst-title",
+					Read:   true,
+				},
+			},
+		} {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				Update(tt.mask, tt.dst, tt.src)
+				assert.DeepEqual(t, tt.expected, tt.dst, protocmp.Transform())
+			})
+		}
+	})
+}
