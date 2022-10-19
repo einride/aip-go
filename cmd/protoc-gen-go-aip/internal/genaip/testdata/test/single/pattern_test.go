99350ba2a15b141a83be814c6a65afd7784b1de2
William Johansson <william.johansson@einride.tech>
2022-10-19 13:08:18 +0200

    chore: add regression tests for multi-pattern resource names

---
+++ cmd/protoc-gen-go-aip/internal/genaip/testdata/test/single/pattern_test.go
@@ -0,0 +1,66 @@
+package single
+
+import (
+	"fmt"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestBookResourceName(t *testing.T) {
+	t.Run("good", func(t *testing.T) {
+		const (
+			shelf = "shelf"
+			book  = "book"
+		)
+		pattern := fmt.Sprintf("shelves/%s/books/%s", shelf, book)
+		var name BookResourceName
+		err := name.UnmarshalString(pattern)
+		assert.NilError(t, err)
+		assert.Equal(t, name.Shelf, shelf)
+		assert.Equal(t, name.Book, book)
+
+		marshalled, err := name.MarshalString()
+		assert.NilError(t, err)
+		assert.Equal(t, marshalled, pattern)
+	})
+
+	t.Run("bad top-level", func(t *testing.T) {
+		var name BookResourceName
+		err := name.UnmarshalString("books/book")
+		assert.Error(t, err, "parse resource name 'books/book' with pattern 'shelves/{shelf}/books/{book}': segment shelves: got books")
+	})
+
+	t.Run("bad wrong parent", func(t *testing.T) {
+		var name BookResourceName
+		err := name.UnmarshalString("others/other/books/book")
+		assert.Error(t, err, "parse resource name 'others/other/books/book' with pattern 'shelves/{shelf}/books/{book}': segment shelves: got others")
+	})
+}
+
+func TestShelfResourceName(t *testing.T) {
+	t.Run("good", func(t *testing.T) {
+		const shelf = "shelf"
+		pattern := fmt.Sprintf("shelves/%s", shelf)
+		name := ShelfResourceName{}
+		err := name.UnmarshalString(pattern)
+		assert.NilError(t, err)
+		assert.Equal(t, name.Shelf, shelf)
+
+		marshalled, err := name.MarshalString()
+		assert.NilError(t, err)
+		assert.Equal(t, marshalled, pattern)
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		name := ShelfResourceName{}
+		err := name.UnmarshalString(fmt.Sprintf("others/other"))
+		assert.Error(t, err, "parse resource name 'others/other' with pattern 'shelves/{shelf}': segment shelves: got others")
+	})
+
+	t.Run("bad wrong parent", func(t *testing.T) {
+		name := ShelfResourceName{}
+		err := name.UnmarshalString(fmt.Sprintf("others/other/shelves/shelf"))
+		assert.Error(t, err, "parse resource name 'others/other/shelves/shelf' with pattern 'shelves/{shelf}': segment shelves: got others")
+	})
+}
