cc2b94f2102fa93f963ca24af5bcb0c851a55b76
Johan <stenehall@users.noreply.github.com>
2022-12-01 15:01:34 +0100

    fix: respect pagination skip field
    BREAKING CHANGE: This will result in a different response if
    next_page_token have been used in combination with skip.

--- pagination/pagetoken_test.go
+++ pagination/pagetoken_test.go
@@ -3,6 +3,7 @@ package pagination
 import (
 	"testing"
 
+	freightv1 "go.einride.tech/aip/proto/gen/einride/example/freight/v1"
 	"google.golang.org/genproto/googleapis/example/library/v1"
 	"gotest.tools/v3/assert"
 )
@@ -34,7 +35,58 @@ func TestParseOffsetPageToken(t *testing.T) {
 		assert.NilError(t, err)
 		assert.Equal(t, int64(30), pageToken3.Offset)
 	})
-
+	t.Run("skip", func(t *testing.T) {
+		t.Run("handle empty token with skip", func(t *testing.T) {
+			request1 := &freightv1.ListSitesRequest{
+				Parent:   "shippers/1",
+				Skip:     30,
+				PageSize: 20,
+			}
+			pageToken1, err := ParsePageToken(request1)
+			assert.NilError(t, err)
+			assert.Equal(t, int64(30), pageToken1.Offset)
+		})
+		t.Run("handle existing token with another skip", func(t *testing.T) {
+			request1 := &freightv1.ListSitesRequest{
+				Parent:   "shippers/1",
+				Skip:     50,
+				PageSize: 20,
+			}
+			pageToken1, err := ParsePageToken(request1)
+			assert.NilError(t, err)
+			assert.Equal(t, int64(50), pageToken1.Offset)
+			request2 := &freightv1.ListSitesRequest{
+				Parent:    "shippers/1",
+				Skip:      30,
+				PageSize:  0,
+				PageToken: pageToken1.String(),
+			}
+			pageToken2, err := ParsePageToken(request2)
+			assert.NilError(t, err)
+			pageToken3 := pageToken2.Next(request2)
+			assert.Equal(t, int64(80), pageToken3.Offset)
+		})
+		t.Run("handle existing token with pagesize and skip", func(t *testing.T) {
+			request1 := &freightv1.ListSitesRequest{
+				Parent:   "shippers/1",
+				Skip:     50,
+				PageSize: 20,
+			}
+			pageToken1, err := ParsePageToken(request1)
+			assert.NilError(t, err)
+			assert.Equal(t, int64(50), pageToken1.Offset)
+			request2 := &freightv1.ListSitesRequest{
+				Parent:    "shippers/1",
+				Skip:      30,
+				PageSize:  20,
+				PageToken: pageToken1.String(),
+			}
+			pageToken2, err := ParsePageToken(request2)
+			assert.NilError(t, err)
+			pageToken3 := pageToken2.Next(request2)
+			assert.Equal(t, int64(100), pageToken3.Offset)
+		})
+	})
 	t.Run("invalid format", func(t *testing.T) {
 		t.Parallel()
 		request := &library.ListBooksRequest{
