3113ede866e48bcaf5e7a61c8d687f2cd362cfed
oscarmuhr <oscar.muhr@einride.tech>
2024-04-12 08:20:13 +0200

    fix(fieldbehavior): traverse messages and collections to clear nested field behaviors TRA-1320

--- fieldbehavior/fieldbehavior_test.go
+++ fieldbehavior/fieldbehavior_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 
 	examplefreightv1 "go.einride.tech/aip/proto/gen/einride/example/freight/v1"
+	syntaxv1 "go.einride.tech/aip/proto/gen/einride/example/syntax/v1"
 	"google.golang.org/genproto/googleapis/api/annotations"
 	"google.golang.org/genproto/googleapis/example/library/v1"
+	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"gotest.tools/v3/assert"
@@ -25,6 +27,272 @@ func TestClearFields(t *testing.T) {
 		assert.Equal(t, site.GetCreateTime(), (*timestamppb.Timestamp)(nil))
 		assert.Equal(t, site.GetDisplayName(), "site one")
 		assert.Equal(t, site.GetName(), "site1")
+	})
+	t.Run("clear field with set field_behavior on nested message", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				Field:           "field",       // has no field_behaviors; should not be cleared.
+				OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+				OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",
+			OptionalField: "optional",
+			MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{
+				Field:         "field",
+				OptionalField: "optional",
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
+	})
+
+	t.Run("clear field with set field_behavior on multiple levels of nested messages", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				Field:           "field",       // has no field_behaviors; should not be cleared.
+				OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+				OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+				MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+					Field:           "field",       // has no field_behaviors; should not be cleared.
+					OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+				},
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",
+			OptionalField: "optional",
+			MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{
+				Field:         "field",
+				OptionalField: "optional",
+				MessageWithoutFieldBehavior: &syntaxv1.FieldBehaviorMessage{
+					Field:         "field",
+					OptionalField: "optional",
+				},
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
+	})
+
+	t.Run("clear fields with set field_behavior on repeated message", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				{
+					Field:           "field",       // has no field_behaviors; should not be cleared.
+					OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+				},
+			},
+			StringList: []string{ // not a message type, should not be traversed
+				"string",
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",
+			OptionalField: "optional",
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{
+				{
+					Field:         "field",
+					OptionalField: "optional",
+				},
+			},
+			StringList: []string{
+				"string",
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
+	})
+
+	t.Run("clear fields with set field_behavior on multiple levels of repeated messages", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				{
+					Field:           "field",       // has no field_behaviors; should not be cleared.
+					OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+					RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+						{
+							Field:           "field",       // has no field_behaviors; should not be cleared.
+							OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+							OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+						},
+					},
+				},
+			},
+			StringList: []string{ // not a message type, should not be traversed
+				"string",
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",
+			OptionalField: "optional",
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{
+				{
+					Field:         "field",
+					OptionalField: "optional",
+					RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{
+						{
+							Field:         "field",
+							OptionalField: "optional",
+						},
+					},
+				},
+			},
+			StringList: []string{
+				"string",
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
+	})
+
+	t.Run("clear repeated field with set field_behavior", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				{
+					Field:         "field",    // has no field_behaviors; should not be cleared.
+					OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+				},
+			},
+			RepeatedOutputOnlyMessage: []*syntaxv1.FieldBehaviorMessage{ // has OUTPUT_ONLY field_behavior; should be cleared.
+				{
+					Field:           "field",
+					OptionalField:   "optional",
+					OutputOnlyField: "output_only",
+				},
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			RepeatedMessage: []*syntaxv1.FieldBehaviorMessage{ // has no field_behaviors; should not be cleared.
+				{
+					Field:         "field",    // has no field_behaviors; should not be cleared.
+					OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+				},
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
+	})
+
+	t.Run("clear fields with set field_behavior on message in map", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			MapOptionalMessage: map[string]*syntaxv1.FieldBehaviorMessage{
+				"key_1": {
+					OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+				},
+			},
+			StringMap: map[string]string{
+				"string_key": "string", // not a message type, should not be traversed
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			OptionalField: "optional",
+			MapOptionalMessage: map[string]*syntaxv1.FieldBehaviorMessage{
+				"key_1": {
+					OptionalField: "optional",
+				},
+			},
+			StringMap: map[string]string{
+				"string_key": "string",
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(
+			t,
+			input,
+			expected,
+			protocmp.Transform(),
+		)
+	})
+
+	t.Run("clear map field with set field_behavior", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			OptionalField: "optional",
+			// has OUTPUT_ONLY field_behavior; should be cleared.
+			MapOutputOnlyMessage: map[string]*syntaxv1.FieldBehaviorMessage{
+				"key_1": {
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; but should be cleared with parent message.
+				},
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			OptionalField: "optional",
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(
+			t,
+			input,
+			expected,
+			protocmp.Transform(),
+		)
+	})
+
+	t.Run("clear field with set field_behavior on oneof message", func(t *testing.T) {
+		t.Parallel()
+		input := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",    // has no field_behaviors; should not be cleared.
+			OptionalField: "optional", // has OPTIONAL field_behavior; should not be cleared.
+			Oneof: &syntaxv1.FieldBehaviorMessage_FieldBehaviorMessage{
+				FieldBehaviorMessage: &syntaxv1.FieldBehaviorMessage{
+					Field:           "field",       // has no field_behaviors; should not be cleared.
+					OptionalField:   "optional",    // has OPTIONAL field_behavior; should not be cleared.
+					OutputOnlyField: "output_only", // has OUTPUT_ONLY field_behavior; should be cleared.
+				},
+			},
+		}
+
+		expected := &syntaxv1.FieldBehaviorMessage{
+			Field:         "field",
+			OptionalField: "optional",
+			Oneof: &syntaxv1.FieldBehaviorMessage_FieldBehaviorMessage{
+				FieldBehaviorMessage: &syntaxv1.FieldBehaviorMessage{
+					Field:         "field",
+					OptionalField: "optional",
+				},
+			},
+		}
+
+		ClearFields(input, annotations.FieldBehavior_OUTPUT_ONLY)
+		assert.DeepEqual(t, input, expected, protocmp.Transform())
 	})
 }
 
